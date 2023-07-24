0a911daba0c9e31a5fb2575c46b0a84f2cd46dae
Papa <papa@vmc03l.localdomain>
2023-07-24 12:16:05 -0700

    update job

--- src/model/train.py
+++ src/model/train.py
@@ -11,7 +11,6 @@ from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
     mlflow.autolog()
 
     # read data
@@ -33,7 +32,6 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
 def split_data(df):
     X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
